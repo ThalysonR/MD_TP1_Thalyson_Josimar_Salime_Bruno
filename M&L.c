ea8b9dde158e3c7e9f1d906c0764a05f7a8f6c76
Thalyson Rocha de Castro <ThalysonR.castro@hotmail.com>
2016-05-11 16:13:00 -0400

    Patch 11/05/2016 16:12

--- M&L.c
+++ M&L.c
@@ -25,6 +25,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct temp{
 		int x,y;
@@ -39,23 +40,66 @@ int aleatorio(int min,int max){
 		return r;
 	}
 
-int main()
-{
-	int m,n,q,num1,num2,i;
-	printf("Informe numero minimo:");
-	scanf("%d",&m);
-	printf("Informe o numero maximo:");
-	scanf("%d",&n);
-	printf("Informe o numero de elementos a serem gerados: ");
-	scanf("%d",&q);
-	for(i=0;i<q;i++){
-		srand(time(NULL));
-		num1 = aleatorio(m,n);
-		sleep(1);
-		num2 = aleatorio(m,n);
-		printf("\nNum1 = %d\nNum2 = %d\n",num1,num2);
+Par *criar(int x,int y){
+		Par *novo = (Par *)malloc(sizeof(Par));
+		novo->x = x;
+		novo->y =y;
+		novo->prox = novo->ant = NULL;
+		return novo;
 	}
 
+void inserir(Par **inicio,Par **fim,int n1,int n2){
+		if(*inicio == NULL){
+				*inicio = *fim = criar(n1,n2);
+		}else{
+			Par *novo = criar(n1,n2);
+			(*fim)->prox = novo;
+			novo->ant = *fim;
+			*fim = novo;
+		}
+	}
+
+void imprimir(Par *inicio){
+		Par *aux = inicio;
+		printf("{");
+		while(aux != NULL){
+			printf("(%d,%d)",aux->x,aux->y);
+			aux = aux->prox;
+		}
+		printf("}");
+	}
+
+int main()
+{
+	int m,n,q,x,y,i;
+	Par *inicio = NULL,*fim;
+	printf("Informe numero minimo:");
+	scanf("%d",&m);
+	if(m >= 0){ //verifica se o numero minimo é positivo
+		printf("Informe o numero maximo:");
+		scanf("%d",&n);
+		n++;
+		if(n-m > 0){ //verifica se o intervalo contém algum elemento
+			printf("Informe o numero de elementos a serem gerados: ");
+			scanf("%d",&q);
+			if(pow(2,(n-m)) >= q){ //verifica se o numero de elementos escolhido é menor que os pares disponiveis no intervalo
+				for(i=0;i<q;i++){ //repete as instruções abaixo por q vezes, definido pelo usuário
+					srand(time(NULL)); //inicializar a função rand -> seed rand
+					x = aleatorio(m,n); //criar um numero aleatório, entre m e n, definidos pelo usuário.
+					sleep(1);
+					y = aleatorio(m,n);
+					inserir(&inicio,&fim,x,y); //insere o par em uma estrutura dinâmica
+				}
+				imprimir(inicio); //imprime todos os pares ordenados
+			}else{
+				printf("Numero Invalido.\n");
+			}
+		}else{
+			printf("Intervalo Invalido.\n");
+		}
+	}else{
+		printf("Numero Invalido.\n");
+	}
 	return 0;
 }
 
